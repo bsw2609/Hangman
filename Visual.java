392fa5a582c5391d0e51f145248555e669856fbb
Brianna Wright <bsw1@williams.edu>
2024-05-05 19:36:18 -0400

    first push for visual

---
+++ Visual.java
@@ -0,0 +1,152 @@
+package tree;
+
+
+public class Visual{
+
+//having something so that when you initalize the game you can see the pole, eerything but the man
+	public static void main(String[] args){
+	System.out.println("Let's begin!");
+	System.out.println("You have five guesses to guess the words, your guessed letterrs will be dispalyed.  Good luck!");
+			System.out.println("	___________");
+			System.out.println("	|			|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("____|____");
+			System.out.println();
+			//System.out.println("You have" + numGuess + "remaining!")
+	}
+
+	public static void hangmanVisual(){
+		//if (numLetters == 1){
+			System.out.println("Incorret guess!");
+			System.out.println("	___________");
+			System.out.println("	|			_|_");
+			System.out.println("	|		   /   \\");
+			System.out.println("	|		  |		|");
+			System.out.println("	|		   \\__//");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("____|____");
+			System.out.println();
+			System.out.println();//print out array of letter space for correct word guess)
+			System.out.println();//print out guessed letters)
+			//System.out.println("You have" + numGuess + "remaining!");
+		//}
+		//if (numLetters == 1){
+			System.out.println("Incorret guess!");
+			System.out.println("	___________");
+			System.out.println("	|			_|_");
+			System.out.println("	|		   /   \\");
+			System.out.println("	|		  |		|");
+			System.out.println("	|		   \\__//");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|");
+			System.out.println("	|");
+			System.out.println("____|____");
+			System.out.println();
+			System.out.println();//print out array of letter space for correct word guess)
+			System.out.println();//print out guessed letters)
+			//System.out.println("You have" + numGuess + "remaining!");
+
+		//if (numLetters == 3){
+			System.out.println("Incorret guess!");
+			System.out.println("	___________");
+			System.out.println("	|			_|_");
+			System.out.println("	|		   /   \\");
+			System.out.println("	|		  |		|");
+			System.out.println("	|		   \\__//");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			/");
+			System.out.println("	|		   /");
+			System.out.println("____|____");
+			System.out.println();
+			System.out.println();//print out array of letter space for correct word guess)
+			System.out.println();//print out guessed letters)
+			//System.out.println("You have" + numGuess + "remaining!");
+
+		//if (numLetters == 3){
+			System.out.println("Incorret guess!");
+			System.out.println("	___________");
+			System.out.println("	|			_|_");
+			System.out.println("	|		   /   \\");
+			System.out.println("	|		  |		|");
+			System.out.println("	|		   \\__//");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			/ \\");
+			System.out.println("	|		   /  \\");
+			System.out.println("____|____");
+			System.out.println();
+			System.out.println();//print out array of letter space for correct word guess)
+			System.out.println();//print out guessed letters)
+			//System.out.println("You have" + numGuess + "remaining!");
+
+		//if (numLetters == 4){
+			System.out.println("Incorret guess!");
+			System.out.println("	___________");
+			System.out.println("	|			_|_");
+			System.out.println("	|		   /   \\");
+			System.out.println("	|		  |		|");
+			System.out.println("	|		   \\__//");
+			System.out.println("	|			 |");
+			System.out.println("	|		    /|");
+			System.out.println("	|		   / |");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			/ \\");
+			System.out.println("	|		   /  \\");
+			System.out.println("____|____");
+			System.out.println();
+			System.out.println();//print out array of letter space for correct word guess)
+			System.out.println();//print out guessed letters)
+			//System.out.println("You have" + numGuess + "remaining!");
+
+
+		//if (numLetters == 5){
+			System.out.println("Incorret guess!");
+			System.out.println("	___________");
+			System.out.println("	|			_|_");
+			System.out.println("	|		   /   \\");
+			System.out.println("	|		  |		|");
+			System.out.println("	|		   \\__//");
+			System.out.println("	|			 |");
+			System.out.println("	|		    /|\\");
+			System.out.println("	|		   / | \\");
+			System.out.println("	|			 |");
+			System.out.println("	|			 |");
+			System.out.println("	|			/ \\");
+			System.out.println("	|		   /  \\");
+			System.out.println("____|____");
+			System.out.println();
+			System.out.println();//print out array of letter space for correct word guess)
+			System.out.println();//print out guessed letters)
+			//System.out.println("You have" + numGuess + "remaining!");
+
+		//condition for wining, print last visual and state that they won the game
+
+	}
+}
